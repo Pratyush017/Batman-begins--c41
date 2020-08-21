e7cf56805afdc73e46ad88089d3135e0a96f635a
Pratyush Raj <60818670+Pratyush017@users.noreply.github.com>
2020-08-21 10:37:00 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Batman-begins--c41
+# PROC41-Template
---
+++ sketch.js
@@ -0,0 +1,29 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+ var raindrops = [];
+ var batman;
+
+function setup() {
+  engine = Engine.create();
+  world = engine.world;
+  var canvas = createCanvas(950,500);
+ batman = new Batman(400, 400, 225);
+
+}
+
+function draw() {
+  background("black");
+  batman.display(); 
+
+  if(frameCount % 1 === 0){
+    var raindrop = new RainDrops(random(0, windowWidth), -100,10);
+    raindrops.push(raindrop);
+  }
+
+  for(var i = 0; i < raindrops.length; i++){
+    raindrops[i].fall(40);
+    raindrops[i].velocityY++
+    raindrops[i].display();
+  }
+}
